4de911a23162579a89f1bc843c386dd69d3b423f
Vulpie <wojtas_f@outlook.com>
2020-09-13 23:19:20 +0200

    feat: add constructors to stack and stack node

---
+++ src/stack/Stack.js
@@ -0,0 +1,11 @@
+const StackNode = require('./StackNode')
+
+class Stack {
+	constructor() {
+		this.top = null
+		this.length = 0
+		this.limit = 0
+	}
+}
+
+module.exports = Stack
---
+++ src/stack/StackNode.js
@@ -0,0 +1,8 @@
+class StackNode {
+	constructor(value, upperNode) {
+		this.value = value
+		this.upperNode = upperNode
+	}
+}
+
+module.exports = StackNode
